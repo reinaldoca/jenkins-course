2ea86ed9aa08ea90eca5f150eafee5e01a369846
reinaldoca <55362209+reinaldoca@users.noreply.github.com>
2021-11-01 13:36:26 -0300

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('reinaldocancino/docker-nodejs-demo-testtest')
+            repositoryName('reinaldocancino/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
